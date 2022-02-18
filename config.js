cdcd34465d78e854db8173dc0b6c03e7d64fb867
Santiago Vermeulen <svermen@gmail.com>
2022-02-17 23:47:31 -0300

    primer commit

---
+++ config.js
@@ -0,0 +1,4 @@
+
+  export const nftmarketaddress = "0x4F518a25f33444D6c59c66bB349d18da266AAd1B"
+  export const nftaddress = "0x870384629bE2c31E823f163aCb006bd50cc35DfC"
+  
---
+++ hardhat.config.js
@@ -0,0 +1,41 @@
+require("@nomiclabs/hardhat-waffle");
+const fs = require('fs');
+ const privateKey = fs.readFileSync(".secret").toString().trim() || "01234567890123456789";
+// const infuraId = fs.readFileSync(".infuraid").toString().trim() || "";
+
+module.exports = {
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      chainId: 1337
+    },
+    BSCtestnet: {
+      url: "https://speedy-nodes-nyc.moralis.io/a2b9b7d1e8a7b5c3b38b6c4d/bsc/testnet",
+      accounts: [privateKey]
+    }
+    /*
+    mumbai: {
+      // Infura
+      // url: `https://polygon-mumbai.infura.io/v3/${infuraId}`
+      url: "https://rpc-mumbai.matic.today",
+      accounts: [privateKey]
+    },
+    matic: {
+      // Infura
+      // url: `https://polygon-mainnet.infura.io/v3/${infuraId}`,
+      url: "https://rpc-mainnet.maticvigil.com",
+      accounts: [privateKey]
+    }
+    */
+  },
+  solidity: {
+    version: "0.8.4",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  }
+};
+
